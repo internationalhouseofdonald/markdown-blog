740af308f26ff56f64156514c3b53731b5f901ec
Donald Moore <donald@maximumdev.biz>
2017-08-03 17:32:12 -0500

    Touching up

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,34 @@
 import '../sass/styles.scss';
 
+// jQuery 3.2.1 (uncompressed)
 $(document).ready(function() {
-    // alert("working");
+    // "iffy"
 })
 
-function toggle() {
-    var toggler = document.getElementById("toggler");
-    toggler.classList.remove('fa-caret-left');
-    toggler.classList.add('fa-caret-down');
-}
+var scroll, togglerIcon, toggleComponent;
+scroll = document.getElementById('scroll');
+togglerIcon = document.getElementById('toggler-icon');
+toggleComponent = document.getElementById('toggle-component');
 
+scroll.addEventListener("click", function() {
+    if ( toggleComponent.classList.contains('collapsed') ) {
+        // TODO: get window width
+        $("html, body").animate({scrollTop: 460}, 1000, "linear");
+    } else {
+        $("html, body").animate({scrollTop: 720}, 1000, "linear");
+    }
+})
+togglerIcon.addEventListener("click", function() {
+    if ( togglerIcon.classList.contains('fa-caret-left') ) {
+    togglerIcon.classList.remove('fa-caret-left');
+    togglerIcon.classList.add('fa-caret-down');
+    } else {
+    togglerIcon.classList.remove('fa-caret-down');
+    togglerIcon.classList.add('fa-caret-left');
+    }
+});
+
+// add Markdown to the div element with id of "content"
 var html = require("../md/wed.md");
 var content = document.getElementById("content");
 content.innerHTML = html;
